0c12aab1cecff2baee59ae8f23c82f02491e131a
shishir <shishir@bookhint.com>
2019-03-11 14:14:36 +0600

    Initial Commit

---
+++ src/Sim.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Sim extends Component {
+  render() {
+    return (
+      <div className="sim">
+        <div>Name: Sim</div>
+        <div>Age: 30</div>
+        <div>belt: Sim</div>
+      </div>
+    );
+  }
+}
+export default Sim
